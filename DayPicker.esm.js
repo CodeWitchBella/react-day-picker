06481b0e07bbc940b792cc50f4a8931d92bdcdc3
Isabella Skořepová <isabella@skorepova.info>
2019-09-20 16:27:53 +0200

    Noninvasive esm module build addition

---
+++ DayPicker.esm.js
@@ -0,0 +1,14 @@
+/*
+  Used as module file in package.json
+*/
+
+import DayPicker from './lib/esm/DayPicker';
+
+export { default as DateUtils } from './lib/esm/DateUtils';
+export { default as LocaleUtils } from './lib/esm/LocaleUtils';
+export { default as ModifiersUtils } from './lib/esm/ModifiersUtils';
+export { default as Weekday } from './lib/esm/Weekday';
+export { default as Navbar } from './lib/esm/Navbar';
+export { default as PropTypes } from './lib/esm/PropTypes';
+
+export default DayPicker;
